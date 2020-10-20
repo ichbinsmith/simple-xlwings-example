700046131790808dc44e35a26a51d94ab6f8a8c1
Smith Djamoura <43211165+ichbinsmith@users.noreply.github.com>
2020-10-21 00:05:55 +0200

    Add files via upload

---
+++ IntroXlwings.py
@@ -0,0 +1,22 @@
+import xlwings as xw
+import pandas as pd
+import os
+
+def main():
+    wb = xw.Book.caller()
+
+    #change working dir : set it to the script dir
+    #dir : os.path.dirname(os.path.realpath(__file__))
+    #file : os.chdir(os.path.dirname(os.path.abspath(__file__))) 
+
+    df = pd.read_csv(os.path.dirname(os.path.realpath(__file__))+"\\data\\iris.csv")
+    df['total_length'] =  df['sepal_length'] + df['petal_length']
+    wb.sheets[0].range('A1').value = df
+
+@xw.func
+def hello(name):
+    return f"Hello {name}!"
+
+if __name__ == "__main__":
+    xw.Book("IntroXlwings.xlsm").set_mock_caller()
+    main()
